70e4752c32a51aade232f6d78fbac16af1f4ef60
lokesh <adobep40@gmail.com>
2020-08-31 07:08:51 -0700

    Updates

---
+++ precache-manifest.af4a134ccf613fc2b04e368e8edb97c9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15019cb251c3dabd0bdc95b76d2b469",
+    "url": "/Whats-Weather/index.html"
+  },
+  {
+    "revision": "34f83dee99d8851e213c",
+    "url": "/Whats-Weather/static/css/2.bd5a7b49.chunk.css"
+  },
+  {
+    "revision": "34f83dee99d8851e213c",
+    "url": "/Whats-Weather/static/js/2.18911bfc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Whats-Weather/static/js/2.18911bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c932e4f9d45dbf0b70",
+    "url": "/Whats-Weather/static/js/main.59213167.chunk.js"
+  },
+  {
+    "revision": "d09efd53022e5f47b29b",
+    "url": "/Whats-Weather/static/js/runtime-main.2d4d9aba.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/Whats-Weather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/Whats-Weather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/Whats-Weather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/Whats-Weather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/Whats-Weather/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
